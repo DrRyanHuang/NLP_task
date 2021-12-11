d2b954386642cb4ef5e65149299ca48a907f661c
lvcunchi <lvcunchi@163.com>
2021-12-12 00:00:10 +0800

    增加LR SVM NB等传统机器学习方法

--- embedding/test.py
+++ embedding/test.py
@@ -3,9 +3,11 @@
 from gensim.models import word2vec
 
 
-if __name__=="__main__":
-    model=word2vec.Word2Vec.load('./embedding/word2vec_100features_10minwords_5win_30epoch.model')
+if __name__ == "__main__":
+    model = word2vec.Word2Vec.load(
+        './embedding/word2vec_100features_10minwords_5win_30epoch.model')
     print(model)
-    print(model.wv.similarity('happy','exciting'))
-    print(model.wv.similarity('happy','sad'))
+    print(model.wv.similarity('happy', 'exciting'))
+    print(model.wv.similarity('happy', 'sad'))
     print(model.wv.most_similar('exciting', topn=10))
+    print(model.wv['happy'])
